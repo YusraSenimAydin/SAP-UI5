b019a864349ac1e5f9836ef3e414f302d6a5ecd4
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-10-03 22:13:36 +0300

    Dialogs and Fragments

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,4 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller"    
-   ], function (Controller) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {    
-
-    });
-  }
-);
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
+});
