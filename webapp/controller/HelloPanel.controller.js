f88b48675fa755c85299c50f9af09a5fb10d2c1b
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-10-03 23:22:44 +0300

    icons

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
-  function (Controller, MessageToast, Fragement) {
+  function (Controller, MessageToast, Fragment) {
     "use strict";
+
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
         // read msg from i18n model
@@ -10,19 +11,20 @@ sap.ui.define(
           .getModel()
           .getProperty("/recipient/name");
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        // Show message
+
+        // show message
         MessageToast.show(sMsg);
       },
+
       onOpenDialog: function () {
         var oView = this.getView();
-
-        // create the dialog lazily
+        //create the dialog lazily
         if (!this.byId("helloDialog")) {
-          // load asynchronous XML fragment
-          Fragement.load({
+          Fragment.load({
+            // load asynchronous XML fragment
             id: oView.getId(),
             name: "sap.ui.demo.walkthrough.view.HelloDialog",
-            controller: this,
+            controller: this
           }).then(function (oDialog) {
             // connect dialog to the root view of this component (models, lifecycle)
             oView.addDependent(oDialog);
@@ -32,7 +34,10 @@ sap.ui.define(
           this.byId("helloDialog").open();
         }
       },
+
       onCloseDialog: function () {
+        // note: We don't need to chain to the pDialog promise, since this event-handler
+        // is only called from within the loaded dialog itself.
         this.byId("helloDialog").close();
       },
     });
