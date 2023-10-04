2e5693333c3a419dba42de10af7af2bece5ae765
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-10-04 17:47:32 +0300

    filtering /sorting/grouping/remoteoDataService

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,20 +1,37 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel",
-  "sap/ui/model/json/JSONModel",
-  "../model/formatter"
-
-],
-  function (Controller, JSONModel , formatter) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/Filteroperator",
+  ],
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-      formatter : formatter,
-      
+      formatter: formatter,
+
       onInit: function () {
         var oViewModel = new JSONModel({
           currency: "EUR",
         });
         this.getView().setModel(oViewModel, "view");
+      },
+      onFilterInvoice: function (oEvent) {
+        //build filter array
+        var aFilters = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductNamer", FilterOperator.Contains, sQuery)
+          );
+        }
+        // filter binding
+        var oList = this.byId("invoiceList");
+        var oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
