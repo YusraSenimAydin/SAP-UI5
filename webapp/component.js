d30b55f6b7d27b387eb8441970bd6f5aa0a6fb28
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-10-04 15:12:43 +0300

    Reuse Dialogs

--- webapp/component.js
+++ webapp/component.js
@@ -3,17 +3,19 @@ sap.ui.define(
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog",
   ],
-  function (UIComponent, JSONModel, ResourceModel) {
+  function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
-          manifest: "json"
+        manifest: "json",
       },
       init: function () {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
         // set data models
+
         var oData = {
           recipient: {
             name: "UI5",
@@ -22,8 +24,17 @@ sap.ui.define(
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
 
-        
-     
+        // set dialog
+        this._helloDialog = new HelloDialog(this.getRootControl());
+      },
+
+      exit: function () {
+        this._helloDialog.destroy();
+        delete this._helloDialog;
+      },
+
+      openHelloDialog: function () {
+        this._helloDialog.open();
       },
     });
   }
--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,13 +1,7 @@
 sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
-<<<<<<< HEAD
-  function (Controller, MessageToast, Fragment) {
-    "use strict";
-
-=======
   function (Controller, MessageToast, Fragement) {
     "use strict";
->>>>>>> 6a4f7c91d44298ab3d27fcdd269f4e66bad99a8b
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
         // read msg from i18n model
@@ -16,54 +10,11 @@ sap.ui.define(
           .getModel()
           .getProperty("/recipient/name");
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-<<<<<<< HEAD
-
-        // show message
-        MessageToast.show(sMsg);
-      },
-
-      onOpenDialog: function () {
-        var oView = this.getView();
-        //create the dialog lazily
-        if (!this.byId("helloDialog")) {
-          Fragment.load({
-            // load asynchronous XML fragment
-            id: oView.getId(),
-            name: "sap.ui.demo.walkthrough.view.HelloDialog",
-            controller: this
-=======
         // Show message
         MessageToast.show(sMsg);
       },
       onOpenDialog: function () {
-        var oView = this.getView();
-
-        // create the dialog lazily
-        if (!this.byId("helloDialog")) {
-          // load asynchronous XML fragment
-          Fragement.load({
-            id: oView.getId(),
-            name: "sap.ui.demo.walkthrough.view.HelloDialog",
-            controller: this,
->>>>>>> 6a4f7c91d44298ab3d27fcdd269f4e66bad99a8b
-          }).then(function (oDialog) {
-            // connect dialog to the root view of this component (models, lifecycle)
-            oView.addDependent(oDialog);
-            oDialog.open();
-          });
-        } else {
-          this.byId("helloDialog").open();
-        }
-      },
-<<<<<<< HEAD
-
-      onCloseDialog: function () {
-        // note: We don't need to chain to the pDialog promise, since this event-handler
-        // is only called from within the loaded dialog itself.
-=======
-      onCloseDialog: function () {
->>>>>>> 6a4f7c91d44298ab3d27fcdd269f4e66bad99a8b
-        this.byId("helloDialog").close();
+        this.getOwnerComponent().openHelloDialog();
       },
     });
   }
